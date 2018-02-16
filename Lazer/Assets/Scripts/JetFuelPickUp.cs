126072819b7aab6fa29e6981cf2db92441226e67
RyanVanDusen <35843705+RyanVanDusen@users.noreply.github.com>
2018-02-16 11:42:17 -0600

    JetPack + PickUps

---
+++ Lazer/Assets/Scripts/JetFuelPickUp.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JetFuelPickUp : MonoBehaviour {
+    [SerializeField]
+    private float fuelPickUpAmount = 100;
+    private JetPack jetpack;
+  
+
+	// Use this for initialization
+	void Start () {
+        jetpack = gameObject.GetComponent<JetPack>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "FuelPickUp")
+        {
+
+            Debug.Log("Picked Up");
+            Destroy(other.gameObject);
+            jetpack.Fuel = fuelPickUpAmount;
+        
+        }
+    }
+}
---
+++ Lazer/Assets/Scripts/JetPack.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JetPack : MonoBehaviour {
+
+    private JetFuelPickUp localFuel;
+    private new Vector3 jetPack;
+
+   
+    [SerializeField]
+    private float upForce = 9.81f;
+    [SerializeField]
+    private float fuel = 100;
+    
+
+    public float Fuel
+    {
+        get
+        {
+            return fuel;
+        }
+        set
+        {
+            fuel = value;
+        }
+    }
+
+
+    
+	// Use this for initialization
+	void Start () {
+        localFuel = new JetFuelPickUp();
+        jetPack = new Vector3(0, upForce * Time.deltaTime, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(fuel);
+        if (Input.GetKey(KeyCode.Space) && fuel > 0)
+        {   
+            this.transform.Translate(jetPack);
+            fuel--;
+           
+        }
+		
+	}
+}
