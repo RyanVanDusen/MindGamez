de9a69394f1c0856d94b9b6ef83d0fbd40b6a76d
Koesterc <Koesterc@gmatc.matc.edu>
2018-02-16 14:06:51 -0600

    Seeking

---
+++ Lazer/Assets/Scanner.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scanner : MonoBehaviour {
+
+	[SerializeField]
+	Transform target;
+
+	[SerializeField]
+	float minClamp;
+	[SerializeField]
+	float maxClamp;
+	[SerializeField]
+	float rotationSpeed;
+
+	float curValue;
+	float min;
+	float max;
+	bool turningRight = true;
+
+	void Start(){
+		curValue = transform.localRotation.y;
+		min = curValue - minClamp;
+		max = curValue + minClamp;
+
+		print (curValue);
+	}
+		
+
+	// Update is called once per frame
+	void Update () {
+		Turning ();
+	}
+
+	public void Turning()
+	{
+		curValue += turningRight ? +rotationSpeed*Time.deltaTime : -rotationSpeed*Time.deltaTime;
+		transform.localRotation = Quaternion.Euler (0, curValue, 0);
+
+			if (curValue >= max) {
+				curValue = max;
+				turningRight = false;
+			}
+			else if (curValue <= min) {
+				curValue = min;
+				turningRight = true;
+			}
+		print (curValue);
+	}
+}
+
+
