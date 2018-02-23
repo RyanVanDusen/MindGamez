62f1dfb1dd133d3a2435d38234aea3f897afabe6
poshepjl <32207101+poshepjl@users.noreply.github.com>
2018-02-23 09:26:37 -0600

    added turret shooting

---
+++ Assets/Scripts/Spin.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    public float speed = 3.0f;
+	
+	void Update ()
+    {
+        //Rotates the object on the Y Axis
+        transform.Rotate(0, speed, 0);
+    }
+}
